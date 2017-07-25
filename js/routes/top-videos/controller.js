9202d570e6fb05a2cae72a9b2553060ef5bcff38
ivanmlaborda <ivanmlaborda@gmail.com>
2017-07-25 16:09:05 +0200

    grid top videos in process

--- js/routes/top-videos/controller.js
+++ js/routes/top-videos/controller.js
@@ -4,8 +4,13 @@ angular.module('skyStream')
 
   DataService.getTopVideos()
   .then(function (oData) {
-    let url = 'https://player.twitch.tv/?video=' + oData.data.videos[0]._id
-    $scope.videos = $sce.trustAsResourceUrl(url)
+    // var url = 'https://player.twitch.tv/?video=' + oData.data.videos
+    // $scope.videos = $sce.trustAsResourceUrl(url)
+    oData.data.videos.forEach(function (key) {
+      var url = 'https://player.twitch.tv/?video=' + key._id + '&autoplay=false'
+      key._id = $sce.trustAsResourceUrl(url)
+    })
+    $scope.videos = oData.data.videos
     console.log($scope.videos)
     console.log('running promise of getTopVideos controller...')
   })
