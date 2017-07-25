8d74abea1b9ef8f5300157e8cdb6668a936fa05b
ivanmlaborda <ivanmlaborda@gmail.com>
2017-07-25 20:01:10 +0200

    location test

--- js/routes/home/controller.js
+++ js/routes/home/controller.js
@@ -1,4 +1,16 @@
 angular.module('skyStream')
-.controller('homeController', function ($scope, DataService) {
+.controller('homeController', function ($scope, $location, DataService) {
   console.log('controller inside home working...')
 })
+
+// angular.module('myGithubApp')
+//   .controller('SearchFormController', function( $scope, $location) {
+
+//     $scope.query = 'juanmaguitar'
+
+//     $scope.getSearchQuery = function() {
+//       var query = $scope.query;
+//       $location.path('/search/' + query);
+//     }
+
+//   })
