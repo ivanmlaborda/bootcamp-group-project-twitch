3de2f34834d305b72f7abdea8e4088950c252ede
David Marin <ddmarin94@gmail.com>
2017-07-25 16:54:55 -0700

    stream videos working

--- js/routes/streams/controller.js
+++ js/routes/streams/controller.js
@@ -1,10 +1,27 @@
 angular.module('skyStream')
-.controller('getStreamsController', function ($scope, DataService) {
+.controller('getStreamsController', function ($scope, DataService, $sce) {
   console.log('controller inside getStreams working...')
 
   DataService.getStreams()
   .then(function (oData) {
+    let url = 'https://player.twitch.tv/?channel=' + oData.data.streams["0"].channel.name
+    $scope.streamVideosName = $sce.trustAsResourceUrl(url)
     console.log(oData)
-    console.log('running promise of getStreams controller...')
+    console.log('running promise of streamVideos controller...')
   })
 })
+
+/* */
+
+// angular.module('skyStream')
+// .controller('getStreamsController', function ($scope, DataService, $sce) {
+//   console.log('controller inside getStreams working...')
+//
+//   DataService.getStreams()
+//   .then(function (oData) {
+//     let url = 'https://player.twitch.tv/?channel=' + oData.data.streams["0"].channel.name
+//     $scope.streamVideosName = $sce.trustAsResourceUrl(url)
+//     console.log(oData)
+//     console.log('running promise of streamVideos controller...')
+//   })
+// })
