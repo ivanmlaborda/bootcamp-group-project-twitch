e90db63e926955aa5c04939027e3915d060e7bba
David Marin <ddmarin94@gmail.com>
2017-07-26 13:42:43 -0700

    remove innecesary css3 files and source maps

--- js/services/dataServices.js
+++ js/services/dataServices.js
@@ -22,13 +22,8 @@ angular.module('skyStream')
     console.log('callingAPI')
   }
 
-<<<<<<< HEAD
-  function searchGames(gameName){
-    var url = 'https://api.twitch.tv/kraken/search/games?query=' + gameName + '&client_id=t0bygtv4nwlrifc9sigricr063nnt1x&type=suggest'
-=======
   function searchGames (query) {
     var url = 'https://api.twitch.tv/kraken/search/games?query=' + query + '&client_id=t0bygtv4nwlrifc9sigricr063nnt1x&type=suggest'
->>>>>>> ede1b918f7d5ec7e994e13850c02a631f3624075
     return $http.get(url)
   }
 
