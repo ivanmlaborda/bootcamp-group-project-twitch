32b978905acc4d54f7ca011798a733c4c72ca4fe
David Marin <ddmarin94@gmail.com>
2017-07-25 22:31:19 -0700

    version without home-pade

--- js/routes/home-page/index2.js
+++
@@ -1,8 +0,0 @@
-angular.module('skyStream')
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/home-page ', {
-    templateUrl: 'js/routes/home page/index.html',
-    controller: 'getHomeController'
-  })
-})
