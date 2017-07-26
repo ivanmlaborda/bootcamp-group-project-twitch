8bf0e36500e862ff9a107a98e0c8efc2c971978a
David Marin <ddmarin94@gmail.com>
2017-07-25 22:27:45 -0700

    wrong version of home page

--- js/routes/home-page/index2.js
+++ js/routes/home-page/index2.js
@@ -2,7 +2,7 @@ angular.module('skyStream')
 .config(function ($routeProvider) {
   $routeProvider
   .when('/home-page ', {
-    templateUrl: '/js/routes/home%20page/js/index.js',
+    templateUrl: 'js/routes/home page/index.html',
     controller: 'getHomeController'
   })
 })
